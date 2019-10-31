f56fc1bfd2fefba0c3f758db8f9222cede10478f
LuanComputacao <eng.luanroger@gmail.com>
2019-10-31 13:27:05 -0300

    starting jwt

---
+++ src/store/modules/auth_jwt.js
@@ -0,0 +1,44 @@
+import henry from '@/api/henry'
+
+// State
+const state = {
+  all: [],
+  grade: null
+}
+
+// Getters
+const getters = {
+  gradeByCode: (state) => (code) => {
+    return state.all.find(grade => grade.code === Number.parseInt(code))
+  }
+}
+
+// Actions
+const actions = {
+  async actionAllGrades ({ commit }) {
+    commit('setGrades', await henry.getGrades())
+  },
+
+  async actionGradeByCode ({ commit }, code) {
+    commit('setGrade', await henry.getGradeByCode(code))
+  }
+}
+
+// Mutations
+const mutations = {
+  setGrades (state, grades) {
+    state.all = grades
+  },
+
+  setGrade (state, grade) {
+    state.grade = grade
+  }
+}
+
+export default {
+  namespaced: true,
+  state,
+  getters,
+  actions,
+  mutations
+}
